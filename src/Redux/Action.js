8fa4d40174d92460de858c3c1441687a65aacb02
Sweta-Swarnakar <68773277+Sweta-Swarnakar@users.noreply.github.com>
2022-04-14 12:13:43 +0530

    deleted

--- src/Redux/Action.js
+++ src/Redux/Action.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import {GET_DOCTOR_FAILURE,GET_DOCTOR_REQ, GET_DOCTOR_SUCCESS,   GET_CLICKED_DOCTOR_REQ, GET_CLICKED_DOCTOR_SUCCESS,
-   FILTER_DATA, SEARCH_TERM} from "./ActionType";
+import {GET_DOCTOR_FAILURE,GET_DOCTOR_REQ, GET_DOCTOR_SUCCESS,   GET_CLICKED_DOCTOR_REQ, GET_CLICKED_DOCTOR_SUCCESS, FILTER_DATA, SEARCH_TERM} from "./ActionType";
 
 
 export const getDoctorRequest = () => ({
@@ -69,7 +68,5 @@ export const getClickedDoctorData = (id) => {
       });
   };
 };
-
-
 
 
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,18 +2,15 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getClickedDoctorData } from "../Redux/Action";
 import { useEffect, useState } from "react";
-import "../styles/Details.css";
-import axios from "axios"
-
+import "../styles/Details.css"
+import axios from "axios";
 
 export const Details = () => {
   const dispatch = useDispatch();
   
-  
-  const current = new Date();
-  var currentTime = `${current.getHours()}`;
-
   var slotCount = 0
+  const current = new Date();
+  const currentTime = `${current.getHours()}`;
 
   
   const state = useSelector((state) => state);
@@ -29,16 +26,17 @@ export const Details = () => {
       if(currentTime >= start && currentTime < end && slots <= 15)
       {
         var temp = Number(slots)
-       // console.log(typeof(temp))
+      
         slotCount = temp - 1
         try {
           const response = await axios.patch(`http://localhost:3000/doctors/${params.id}`, { slots: slotCount });
           console.log('👉 Returned data:', response.data);
+          window.alert(`Booking confirmed. Slots left : ${response.data.slots}`)
          
         } catch (e) {
           console.log(`😱 Axios request failed: ${e}`);
         }
-        window.alert("Booking confirmed")
+       
       }
 
       else if(currentTime < start)
@@ -65,18 +63,27 @@ export const Details = () => {
   return (
     <>
      
-      <h1>Doctor's Details Page</h1>;
+      <h1></h1>;
       <div >
         <div className="imgBox">
           <img
             src={state.selectedData.img}
             alt=""
-            
+            height="100%"
+            width="100%"
           />
         </div>
         <div className="infoBox">
           <h1>{state.selectedData.firstName} {state.selectedData.lastName}</h1>
-         
+          {state?.selectedData?.category &&
+            state.selectedData.category
+              .trim()
+              .split("|")
+              .map((el, index) => (
+                <p key={index}>
+                  {el}
+                </p>
+              ))}
           <p>
             <b>Experince:</b> {state.selectedData.exp}
           </p>
@@ -87,15 +94,12 @@ export const Details = () => {
           <p>
             <b>Time:</b> {state.selectedData.startTime} to {state.selectedData.endTime} (24 hours Format)
           </p>
-
-          <p>
-            <b>Avaible Slots:</b> {state.selectedData.slots}
-          </p>
-         
+          {
+            // if(currentTime >= {state.selectedData.startTime} && currentTime < state.selectedData.endTime)
             
-              <button className="appointment"  onClick={(e) => handleBook( state.selectedData.startTime, state.selectedData.endTime, state.selectedData.slots)}>Book Appointment</button>
-          
-          
+            <button className="appointment"  onClick={(e) => handleBook( state.selectedData.startTime, state.selectedData.endTime, state.selectedData.slots)}>Book Appointment</button>
+            
+          }
           
           
         
